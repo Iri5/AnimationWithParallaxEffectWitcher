56ee42aa215c259f19479ce2736214b5dc084070
Iri5 <bovchurova-irina@rambler.ru>
2023-06-04 19:15:40 +0300

    two slides work

--- app.js
+++ app.js
@@ -1,5 +1,33 @@
+let blocks = document.querySelectorAll('.block');
+let count = blocks.length;
+console.log(blocks);
 window.addEventListener('scroll', e =>{
     document.body.style.cssText += `--scrollTop: ${this.scrollY}px`
+    let windowInnerHeight = document.documentElement.clientHeight;
+    if (this.scrollY > windowInnerHeight){
+        document.body.style.cssText += `--scrollTop: ${this.scrollY - windowInnerHeight}px`
+    }
+   
+    for (let i = 0; i < count; i++){
+        if (this.scrollY > windowInnerHeight * (i+1)){
+            let checkBlock = document.querySelectorAll('.block');
+            if (!checkBlock[i].firstElementChild.firstElementChild.classList.contains('layers__base')){
+                checkBlock[i].firstElementChild.firstElementChild.classList.add('layers__base');
+                checkBlock[i].firstElementChild.firstElementChild.nextElementSibling.classList.add('layers__middle');
+                checkBlock[i].firstElementChild.firstElementChild.nextElementSibling.classList.add('layers__front');
+            }
+        }
+        if (this.scrollY < windowInnerHeight * (i+1)){
+            let checkBlock = document.querySelectorAll('.block');
+            if (checkBlock[i].firstElementChild.firstElementChild.classList.contains('layers__base')){
+                checkBlock[i].firstElementChild.firstElementChild.classList.remove('layers__base');
+                checkBlock[i].firstElementChild.firstElementChild.nextElementSibling.classList.remove('layers__middle');
+                checkBlock[i].firstElementChild.firstElementChild.nextElementSibling.classList.remove('layers__front');
+            }
+        }
+    }
+    
+    
 }
 )
 gsap.registerPlugin(ScrollTrigger, ScrollSmoother)
